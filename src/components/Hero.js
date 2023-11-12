0f7bcf3c4ba3407efe8e9595e82977caf2d6ca2a
Ayushman Satpathy <ayushmansatpathy1234@gmail.com>
2023-11-12 03:04:42 -0500

    made some changes to API call

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,14 +3,16 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 // import logo from "../assets/logo.svg";
 import umass from '../assets/umass.png';
-
+const id = ""
 const Hero = () => {
   const { user, isAuthenticated } = useAuth0();
   return (
     <div className="text-center hero my-5">
       <img className="mb-3 app-logo" src={umass} alt="UMass logo" width="120" />
       <h1 className="mb-4">UMass Badminton Portal</h1>
-      <div>{isAuthenticated ? "Yes" : "No"}</div>
+
+      <div>{isAuthenticated ? addUser(user) : "No"}</div>
+      <div></div>
       {/* <div>{user.name}</div> */}
       {isAuthenticated && (
         <h1>
@@ -24,3 +26,29 @@ const Hero = () => {
   )
 }
 export default Hero;
+
+const addUser = (user) => {
+  try {
+    const response = fetch('https://fastapi-backend-fl6pmqzvxq-uc.a.run.app/matches/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: user.name,
+        email: user.email,
+      }),
+    });
+
+    if (response.ok) {
+      // Request was successful
+      console.log('Human created successfully');
+      console.log(response.json.name)
+    } else {
+      // Handle errors
+      console.error('Error scheduling appointment');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
--- src/components/badminton/cals/cal.jsx
+++ src/components/badminton/cals/cal.jsx
@@ -4,7 +4,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 const SchedulingComponent = ({ courtNumber, bookedTimes, onBooking }) => {
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [selectedTime, setSelectedTime] = useState('');
+    const [userdata, setuserdata] = useState("")
+    const [bookingdata, setBookingData] = useState("")
+    const url1 = "https://fastapi-backend-fl6pmqzvxq-uc.a.run.app"
+    const url2 = "http://127.0.0.1:8000"
     const { user } = useAuth0();
+    const user_email = user.email
     let currentDate = new Date()
     const handleDateChange = (date) => {
         setSelectedDate(date);
@@ -18,26 +23,33 @@ const SchedulingComponent = ({ courtNumber, bookedTimes, onBooking }) => {
 
     const handleScheduleAppointment = async () => {
         try {
-            const response = await fetch('https://fastapi-backend-fl6pmqzvxq-uc.a.run.app/matches/users/', {
+            const response = await fetch(`${url2}/matches/bookings/`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    booked: false,
-                    email: user.email,
-                    name: user.name,
-                    date: selectedDate.toISOString().split('T')[0],
-                    time: selectedTime,
+                    day: selectedDate.getDate(),
+                    month: selectedDate.getMonth(),
+                    year: selectedDate.getFullYear(),
+                    start: parseInt(selectedTime),
+                    end: parseInt(selectedTime) + 1,
+                    court: courtNumber,
                 }),
             });
 
             if (response.ok) {
+
                 // Request was successful
                 console.log('Appointment scheduled successfully');
                 console.log(selectedTime)
+                // await getBookingID(response)
+                await getUser()
                 // Notify parent component about the booking
                 onBooking(courtNumber, `${selectedDate.toISOString().split('T')[0]} ${selectedTime}`);
+                // console.log(bookingdata)
+                console.log(userdata)
+
             } else {
                 // Handle errors
                 console.error('Error scheduling appointment');
@@ -51,15 +63,59 @@ const SchedulingComponent = ({ courtNumber, bookedTimes, onBooking }) => {
         setSelectedTime('');
     };
 
+    const getBookingID = async (p) => {
+        const data = await p.json()
+        setBookingData(data)
+    }
+
+    const getUser = async () => {
+        const response = await fetch(`${url2}/matches/users/${user_email}`)
+        if (response.ok) {
+            const data = await response.json()
+            setuserdata(data)
+        } else {
+            console.log(response.json)
+        }
+    }
+
+    const handleUpdateUser = async (booking_id) => {
+
+        try {
+            const response = await fetch(`${url2}/matches/users/${userdata.id}/`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    booking_id: booking_id
+                }),
+            });
+
+            if (response.ok) {
+                // Request was successful
+                console.log('User updated successfully');
+            } else {
+                console.error('Error updating user');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
     const handlePostRequest = async () => {
         try {
-            const response = await fetch('https://fastapi-backend-fl6pmqzvxq-uc.a.run.app/matches/users');
+            const response = await fetch('https://fastapi-backend-fl6pmqzvxq-uc.a.run.app/matches/users/');
             const data = await response.json();
             console.log(data);
         } catch (error) {
             console.error('Error:', error);
         }
     };
+
+    const finalfunc = async () => {
+        await handleScheduleAppointment()
+        // await handleUpdateUser()
+    }
 
     return (
         <div>
@@ -97,7 +153,7 @@ const SchedulingComponent = ({ courtNumber, bookedTimes, onBooking }) => {
                 </select>
             </div>
 
-            <button onClick={handleScheduleAppointment} disabled={!selectedDate || !selectedTime || bookedTimes.includes(`${selectedDate.toISOString().split('T')[0]} ${selectedTime}`)}>
+            <button onClick={finalfunc} disabled={!selectedDate || !selectedTime || bookedTimes.includes(`${selectedDate.toISOString().split('T')[0]} ${selectedTime}`)}>
                 Schedule Appointment
             </button>
         </div>
